acfc16f9bf7d9300142d37e6048139c5e56927ba
Yuekang Li <yli044@e.ntu.edu.sg>
2024-01-08 00:02:50 +1100

    minors

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import { Card, Col, Row, Divider, Typography, Space, Avatar, Button } from 'antd';
-import {HomeOutlined, TwitterOutlined, GithubOutlined} from '@ant-design/icons';
+import {HomeFilled, TwitterOutlined, GithubOutlined} from '@ant-design/icons';
 
 const { Meta } = Card;
 
@@ -59,18 +59,19 @@ function FounderCard({name, intro, photo, home, twitter, github, title, affiliat
   return (
     <Card hoverable bordered={false}
           actions={[
-            <Button type="text" href={home} icon={<HomeOutlined />} />,
-            <Button type="text" href={github} icon={<GithubOutlined />} />,
-            <Button type="text" href={twitter} icon={<TwitterOutlined />} />
+            <a href={home}> <HomeFilled /> </a>,
+            // <Button type="text" href={home} icon={<HomeOutlined />} />,
+            <a href={github}> <GithubOutlined /> </a>,
+            <a href={twitter}> <TwitterOutlined /> </a>
           ]}>
       <Meta
         avatar={<Avatar src={photo}/>}
         title={name}
         description={
           <div>
-            <p><strong>{title} @ {affiliation}</strong></p>
+            <p style={{minHeight: "50px"}}><strong>{title} @ {affiliation}</strong></p>
             <Divider/>
-            <p>{intro}</p>
+            <p style={{minHeight: "100px"}}>{intro}</p>
           </div>
         }
       />
@@ -102,7 +103,7 @@ export default function HomepageFeatures() {
         {/*  ))}*/}
         {/*</div>*/}
         <Title>Introduction</Title>
-        <p>
+        <p style={{color: "black"}}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis, turpis at consequat varius, elit nibh pulvinar metus, convallis commodo libero ipsum vel nibh. Vivamus vestibulum neque sem, id molestie dui fermentum vel. Suspendisse volutpat dui bibendum, molestie nulla eu, venenatis purus. Suspendisse fringilla nunc nisi, non pretium dolor lobortis eget. Maecenas eleifend nulla quis tempor semper. Proin eros risus, sodales non fringilla egestas, congue quis ipsum. Nulla facilisi. Cras posuere, arcu non imperdiet elementum, dolor dui molestie diam, a scelerisque neque enim non odio. In eu erat id dolor accumsan ultrices ac eget nibh. Donec ac arcu ex.
 
           Nulla ut felis sit amet nulla sagittis dignissim eu ut arcu. Ut tristique justo mauris. Mauris in justo leo. Aliquam odio risus, maximus in tortor porta, ultrices congue arcu. Aenean vitae dignissim odio, at vehicula felis. Etiam non imperdiet orci. Nullam dui ligula, volutpat in accumsan ac, venenatis tempus leo. Sed urna libero, facilisis et venenatis bibendum, tristique at libero. Nunc dui nulla, viverra quis aliquet nec, mollis vel eros. Mauris elementum accumsan leo ut lobortis. Vivamus auctor nec arcu quis sagittis. Pellentesque in lectus vel nibh pellentesque dignissim et eget risus.
